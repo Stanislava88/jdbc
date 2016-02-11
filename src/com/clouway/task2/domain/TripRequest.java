f19723690ee4ba020331ab17feb68a96ba9dc82c
slavidichkov <dichkof@gmail.com>
2016-02-11 14:52:52 +0200

    delete get methods

--- src/com/clouway/task2/domain/TripRequest.java
+++ src/com/clouway/task2/domain/TripRequest.java
@@ -4,19 +4,11 @@ package com.clouway.task2.domain;
  * @author Slavi Dichkov (slavidichkof@gmail.com)
  */
 public class TripRequest {
-    private final Person person;
-    private final Trip trip;
+    public final Person person;
+    public final Trip trip;
 
     public TripRequest(Person person, Trip trip){
         this.person = person;
         this.trip = trip;
-    }
-
-    public Person getUser() {
-        return person;
-    }
-
-    public Trip getTrip() {
-        return trip;
     }
 }
