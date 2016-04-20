93a28aed59b6c00204a3513c0093567e4591e51d
Stanislava Kaukova <sisiivanovva@gmail.com>
2016-04-20 11:49:46 +0300

    Change name to registerMultiple

--- tests/com/clouway/sqlcommands/StudentRepositoryTest.java
+++ tests/com/clouway/sqlcommands/StudentRepositoryTest.java
@@ -46,7 +46,7 @@ public class StudentRepositoryTest {
   }
 
   @Test
-  public void registerMultipleStudent() throws Exception {
+  public void registerMultiple() throws Exception {
     Student student1 = new Student(1, "Lilia", "Angelova", 24);
     Student student2 = new Student(2, "Katia", "Ivanova", 25);
 
